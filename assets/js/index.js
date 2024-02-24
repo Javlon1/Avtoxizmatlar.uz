f323491ba71537405a25c60b1fa20893fe74330f
Javlon1 <muhamedmijotot@gmail.com>
2024-02-24 19:23:22 +0500

    home page end

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,24 +25,36 @@ document.addEventListener('DOMContentLoaded', function () {
 // Scroll Up end
 
 // nav start
-const menu = document.getElementById("menu")
-const nav = document.querySelector(".nav__item")
-const navLog = document.querySelector(".nav__log")
-const opacity = document.querySelector(".opacity")
+const menu = document.getElementById("menu");
+const nav = document.querySelector(".nav__item");
+const navLog = document.querySelector(".nav__log");
+const opacity = document.querySelector(".opacity");
 
 menu.onclick = () => {
-    menu.classList.toggle("openmenu")
-    nav.classList.toggle("active_nav")
-    navLog.classList.toggle("active_nav")
-    opacity.classList.toggle("opacity_act")
-    document.body.style.overflow = document.body.style.overflow === 'hidden' ? '' : 'hidden'; 
-}
+    menu.classList.toggle("openmenu");
+    nav.classList.toggle("active_nav");
+    navLog.classList.toggle("active_nav");
+    opacity.classList.toggle("opacity_act");
+    document.body.style.overflow = document.body.style.overflow === 'hidden' ? 'auto' : 'hidden';
+};
 
 opacity.onclick = () => {
-    menu.classList.remove("openmenu")
-    nav.classList.remove("active_nav")
-    navLog.classList.remove("active_nav")
-    opacity.classList.remove("opacity_act")
+    menu.classList.remove("openmenu");
+    nav.classList.remove("active_nav");
+    navLog.classList.remove("active_nav");
+    opacity.classList.remove("opacity_act");
     document.body.style.overflow = 'auto';
-}
+};
 // nav end
+
+// Dropdown Start
+document.getElementById("myInput").addEventListener("focus", function () {
+    document.getElementById("myDropdown").classList.add("show");
+    opacity.classList.add("opacity_act");
+    document.body.style.overflow = 'hidden';
+});
+
+document.getElementById("myInput").addEventListener("blur", function () {
+    document.getElementById("myDropdown").classList.remove("show");
+});
+// Dropdown End
