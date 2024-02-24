07e25f029200258ac113777cb54fadf7e443b8cf
Javlon1 <muhamedmijotot@gmail.com>
2024-02-24 14:57:50 +0500

    start

---
+++ assets/js/index.js
@@ -0,0 +1,46 @@
+// Scroll Up Start
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollUpButton = document.getElementById('scrollUp');
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        const threshold = 100;
+        if (scrollY > threshold) {
+            scrollUpButton.classList.add('visible');
+        } else {
+            scrollUpButton.classList.remove('visible');
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    scrollUpButton.addEventListener('click', scrollToTop);
+});
+// Scroll Up end
+
+// nav start
+const menu = document.getElementById("menu")
+const nav = document.querySelector(".nav__item")
+const navLog = document.querySelector(".nav__log")
+const opacity = document.querySelector(".opacity")
+
+menu.onclick = () => {
+    menu.classList.toggle("openmenu")
+    nav.classList.toggle("active_nav")
+    navLog.classList.toggle("active_nav")
+    opacity.classList.toggle("opacity_act")
+}
+
+opacity.onclick = () => {
+    menu.classList.remove("openmenu")
+    nav.classList.remove("active_nav")
+    navLog.classList.remove("active_nav")
+    opacity.classList.remove("opacity_act")
+}
+// nav end
