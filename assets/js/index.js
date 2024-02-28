258f2126d0d4be02116406330ac2d839dfca37f3
Javlon1 <muhamedmijotot@gmail.com>
2024-02-28 16:19:19 +0500

    settings

--- assets/js/index.js
+++ assets/js/index.js
@@ -125,3 +125,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 //Akkardion End 
+
+// Input Pattren Start
+document.getElementById('phoneInput').addEventListener('input', function (e) {
+    let x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})(\d{0,2})/);
+    e.target.value = !x[2] ? x[1] : x[1] + ' ' + x[2] + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '') + (x[5] ? ' ' + x[5] : '') + (x[6] ? ' ' + x[6] : '');
+});
+// Input Pattren End
